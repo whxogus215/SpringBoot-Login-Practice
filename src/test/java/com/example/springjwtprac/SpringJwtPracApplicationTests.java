bdaf3d0d804844d6a21388344ac6ebeae2a3605e
whxogus215 <whxogus215@naver.com>
2024-01-09 22:06:39 +0900

    build: 스프링 시큐리티 및 JWT 등 의존성 추가
    - JPA 관련 의존성 임시 주석 처리

---
+++ src/test/java/com/example/springjwtprac/SpringJwtPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjwtprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
